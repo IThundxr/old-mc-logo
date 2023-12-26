6ce4d4cca1d00a9f1acc8f409d906b4a0a7f5133
IThundxr <contact@ithundxr.dev>
2023-12-26 14:07:17 -0500

    1.20.3/dev

--- forge/src/main/java/dev/ithundxr/mods/oldmclogo/forge/OldMCLogoForge.java
+++
@@ -1,7 +0,0 @@
-package dev.ithundxr.mods.oldmclogo.forge;
-
-import dev.ithundxr.mods.oldmclogo.OldMCLogo;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(OldMCLogo.MOD_ID)
-public class OldMCLogoForge {}
